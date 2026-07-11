42fab8d1e908af5eb56c3db85f2a4dfbc21a5abc
Heer972005 <mehtaheer.972@gmail.com>
2026-07-11 13:31:38 +0530

    done with patterns

--- notepad/patterns.cpp
+++ notepad/patterns.cpp
@@ -210,6 +210,104 @@ void print17(int n){
 	}
 }
 
+
+void print18(int n){
+	for(int i=0;i<n;i++){
+		char ch='E'-i;
+		for(int j=0;j<=i;j++){
+			cout<<ch;
+			ch=ch+1;
+		}
+		cout<<endl;
+	}
+}
+
+void print19(int n){
+	int space=2*n;
+	for(int i=1;i<=2*n;i++){
+		int breakpoint=n+1;
+		if(i<breakpoint){
+			for(int j=1;j<=n-i+1;j++){
+				cout<<"*";
+			}
+			for(int j=1;j<=2*i-2;j++){
+				cout<<" ";
+			}
+			for(int j=1;j<=n-i+1;j++){
+				cout<<"*";
+			}
+		}
+		else{
+			for(int j=1;j<=i-n;j++){
+				cout<<"*";
+			}
+			for(int j=1;j<=space-2;j++){
+				cout<<" ";
+			}
+			for(int j=1;j<=i-n;j++){
+				cout<<"*";
+			}
+			space=space-2;
+		}
+		cout<<endl;
+	}
+}
+
+void print20(int n){
+	int spce=2*n;
+	for(int i=1;i<=2*n-1;i++){
+		if((n+1)>i){
+			for(int j=1;j<=i;j++){
+				cout<<"*";
+			}
+			for(int j=1;j<=spce-2;j++){
+				cout<<" ";
+			}
+			spce=spce-2;
+			for(int j=1;j<=i;j++){
+				cout<<"*";
+			}
+		}
+		else{
+			for(int j=1;j<=2*n-i;j++){
+				cout<<"*";
+			}
+			for(int j=1;j<=(i-n)*2;j++){
+				cout<<" ";
+			}
+			for(int j=1;j<=2*n-i;j++){
+				cout<<"*";
+			}
+			
+		}
+		cout<<endl;
+	}
+}
+
+void print21(int n){
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			if(j==0||i==0||i==n-1||j==n-1)
+				cout<<"*";
+			else
+				cout<<" ";
+		}
+		cout<<endl;
+	}
+}
+
+void print22(int n){
+	for(int i=0;i<2*n-1;i++){
+		for(int j=0;j<2*n-1;j++){
+			int top=i;
+			int left=j;
+			int right=(2*n-2)-j;
+			int bottom=(2*n-2)-i;
+			cout<<(n-min(min(top,bottom),min(left,right)));
+		}
+		cout<<endl;
+	}
+}
 int main(){
 	int n;
 	cin>>n;
@@ -246,5 +344,14 @@ int main(){
 	print16(n);
 	cout<<endl;
 	print17(n);
-
+	cout<<endl;
+	print18(n);
+	cout<<endl;
+	print19(n);
+	cout<<endl;
+	print20(n);
+	cout<<endl;
+	print21(n);
+	cout<<endl;
+	print22(n);
 }
