be5c6313481326e2fb6cd9c03f8541754c157c54
Heer972005 <mehtaheer.972@gmail.com>
2026-07-11 13:59:47 +0530

    count digits

---
+++ notepad/basic_mth/countdigits.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int count(int n){
+	int count=0;
+	while(n>0){
+		count=count+1;
+		n=n/10;
+	}
+	return count;
+}
+
+int count_log(int n){
+	int count=(int)(log10(n)+1);
+	return count;
+}
+
+int main(){
+	int n;
+	cin>>n;
+	int count1=count(n);
+	int count2=count_log(n);
+	cout<<count1;
+	cout<<count2;
+}
