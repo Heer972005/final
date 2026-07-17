050499603c53c84e87b7ef067c6991877f07cfaa
Heer972005 <mehtaheer.972@gmail.com>
2026-07-17 18:36:16 +0530

    done with if Sorted

---
+++ notepad/array/ifSorted.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isSorted(int n,vector<int>a)
+{
+	for(int i=1;i<n;i++)
+	{
+		if(a[i]>=a[i-1])
+		{}
+		else
+			return false;
+	}
+	return true;
+}
+int main()
+{
+	int n;
+	cin>>n;
+	vector<int>arr(n);
+	for(int i=0;i<n;i++)
+		cin>>arr[i];
+	cout<<isSorted(arr.size(),arr);
+	return 0;
+}
